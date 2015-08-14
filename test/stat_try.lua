3614732858802432e736c0f30818436119387d77
Masatoshi Teruya <mah0x211@gmail.com>
2015-08-15 03:16:47 +0900

    change: test/stat_try.lua has been updated.

--- test/stat_try.lua
+++ test/stat_try.lua
@@ -5,9 +5,19 @@ ifNotEqual( stat.type, 'reg' );
 
 stat = ifNil( path.stat( './' ) );
 ifNotEqual( stat.type, 'dir' );
+ifNotNil( stat.fd );
 
 stat = ifNil( path.stat( './', false, false ) );
 ifNotNil( stat.type );
+ifNotNil( stat.fd );
+
+stat = ifNil( path.stat( './', false, nil ) );
+ifNil( stat.type );
+ifNotNil( stat.fd );
+
+stat = ifNil( path.stat( 'stat_try.lua', false, true, true ) );
+ifNil( stat.type );
+ifNil( stat.fd );
 
 ifNotNil( path.stat( 'stat_try.lua_' ) );
 
